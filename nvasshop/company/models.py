17051c0985c115c035a7f2b36f8efb214940deb6
Breskva <vukmacanovic@gmail.com>
2023-11-19 09:54:02 +0100

    Added equipment type enum

--- nvasshop/company/models.py
+++ nvasshop/company/models.py
@@ -12,11 +12,23 @@ class Company(models.Model):
     rate = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
 
 
+class EquipmentType(models.IntegerChoices):
+    DIAGNOSTIC_EQUIPMENT = 1, 'Diagnostic Equipment'
+    MONITORING_EQUIPMENT = 2, 'Monitoring Equipment'
+    SURGICAL_EQIPMENT = 3, 'Surgical Equipment'
+    LABORATORY_EQUIPMENT = 4, 'Laboratory Equipment'
+    PATIENT_CARE_EQUIPMENT = 5, 'Patient Care Equipment'
+    ORTHOPEDIC_EQUIPMENT = 6, 'Orthopedic Equipment'
+
 class Equipment(models.Model):
     company = models.ForeignKey(Company, related_name='equipment', on_delete=models.CASCADE, default=0)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     quantity = models.IntegerField(default=0)
+    type = models.IntegerField(
+        choices=EquipmentType.choices,
+        default=EquipmentType.DIAGNOSTIC_EQUIPMENT,
+    )
 
 class PickupSchedule(models.Model):
     company = models.ForeignKey(Company, related_name='pickup_schedules', on_delete=models.CASCADE, default=0)
