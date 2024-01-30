1c1ba33837dd9bcafab0e95b54fd0fda1e2636b2
aleksandarm001 <aleksandar.marinkovic6366@gmail.com>
2024-01-30 17:19:29 +0100

    fixed celery task registration

--- nvasshop/nvasshop/celery_setup.py
+++ nvasshop/nvasshop/celery_setup.py
@@ -3,7 +3,6 @@ import os
 from celery import Celery
 from celery import shared_task
 
-from nvasshop.tasks import CheckPickupSchedule
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nvasshop.settings')
--- nvasshop/nvasshop/tasks.py
+++ nvasshop/nvasshop/tasks.py
@@ -1,29 +1,29 @@
 from datetime import datetime
-
-import celery
 from celery import shared_task
 from django.db import transaction
-from django.apps import apps
+
+from company.models import EquipmentReservation, PickupSchedule
+from user.models import User
+import logging
 
 
-class CheckPickupSchedule(celery.Task):
-    def run(self):
-        print("Checking for past pickup schedules...")
-        now = datetime.now()
-        try:
-            with transaction.atomic():
-                EquipmentReservation = apps.get_model('company', 'EquipmentReservation')
-                PickupSchedule = apps.get_model('company', 'PickupSchedule')
-                User = apps.get_model('user', 'User')
-
-                pending_reservations = EquipmentReservation.objects.filter(status='pending')
-                for reservation in pending_reservations:
-                    pickup_schedule = PickupSchedule.objects.get(id=reservation.pickup_schedule_id)
-                    if pickup_schedule.date < now:
-                        reservation.status = 'rejected'
-                        reservation.save()
-                        user = User.objects.get(id=reservation.user_id)
-                        user.penal_amount += 2
-                        user.save()
-        except (EquipmentReservation.DoesNotExist, PickupSchedule.DoesNotExist) as e:
-            print(f"Error occurred: {e}")
+@shared_task
+def check_past_pickup_schedules():
+    logger = logging.getLogger(__name__)
+    logger.info("Starting check_past_pickup_schedules task")
+    print("Checking for past pickup schedules...")
+    now = datetime.now()
+    try:
+        with transaction.atomic():
+            pending_reservations = EquipmentReservation.objects.filter(status='pending')
+            for reservation in pending_reservations:
+                pickup_schedule = PickupSchedule.objects.get(id=reservation.pickup_schedule_id)
+                if pickup_schedule.date < now:
+                    reservation.status = 'rejected'
+                    reservation.save()
+                    user = User.objects.get(id=reservation.user_id)
+                    user.penal_amount+=2
+                    user.save()
+        logger.info("Finished check_past_pickup_schedules task")
+    except (EquipmentReservation.DoesNotExist, PickupSchedule.DoesNotExist) as e:
+        print(f"Error occurred: {e}")
