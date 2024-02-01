f56c861789acd9887ef1edb23c362b76fa4cb171
Breskva <vukmacanovic@gmail.com>
2024-02-01 01:54:35 +0100

    Location simulator fully implemented

--- locationSimulator/simulator.py
+++ locationSimulator/simulator.py
@@ -26,30 +26,39 @@ def send_to_map(start, end, channel, updateFrequency):
 
     # Google Maps Directions API endpoint
     api_endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-    request_url = f'{api_endpoint}origin={start}&destination={end}&key={api_key}&overview_polyline=true'
+    request_url = f'{api_endpoint}origin={start[0]},{start[1]}&destination={end[0]},{end[1]}&key={api_key}&overview_polyline=true'
     coordinates = get_route_coordinates(request_url)
  
-    coordinates.append({'latitude': 0, 'longitude': 0})
-
-    channel.basic_consume(queue='queue1', on_message_callback=callback, auto_ack=True)
-
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+    coordinates.append((0, 0))
 
     print(coordinates)
+    coordinate = coordinates.pop(0)
+    coordinate_dict = {"latitude": coordinate[0], "longitude": coordinate[1]}
+    channel.basic_publish(exchange='', routing_key='queue2', body=json.dumps(coordinate_dict))
+    time.sleep(updateFrequency)
     while(len(coordinates) > 0):
-        channel.basic_publish(exchange='', routing_key='queue2', body=json.dumps(coordinates.pop(0)))
+        popCount = updateFrequency*2
+        while (popCount > 0 and len(coordinates) > 2):
+            coordinates.pop(0)
+            popCount -= 1
+        coordinate = coordinates.pop(0)
+        coordinate_dict = {"latitude": coordinate[0], "longitude": coordinate[1]}
+        channel.basic_publish(exchange='', routing_key='queue2', body=json.dumps(coordinate_dict))
         time.sleep(updateFrequency)
 
 
 def get_route_coordinates(request_url):
+    #print(request_url)
     response = requests.get(request_url)
+
+    #print("request:", request_url)
 
     # Dobijanje rute između početne i krajnje tačke
     if response.status_code == 200:
         # Parse the response JSON
         data = response.json()
 
+        #print(data)
         # Extract the 'overview_polyline'
         overview_polyline = data.get('routes', [{}])[0].get('overview_polyline', {}).get('points', '')
 
